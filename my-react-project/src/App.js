65fc6e1ce8675f292c0de3328adca3dd4e8fb02f
Tomoya <ichikawatomoya0705@gmail.com>
2024-11-06 23:56:05 +0900

    基本的なスタイルのあて方

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,30 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useCallback } from "react"; 
-import { ChildArea } from './Components/ChildArea';
+import{ InlineStyle } from "./Components/InlineStyle"
+import "./styles.css";
 
-function App() {
-  console.log("Appがレンダリングされた！");
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => setText(e.target.value);
-
-  const onClickOpen = () => setOpen(!open);
-
-  const onClickClose = useCallback(() => setOpen(false), []);
-
+export default function App() {
   return (
-    <div className='App'>
-      <input value={text} onChange={onChangeText} />
-      <p>{text}</p>
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
+    <div className="App">
+      <InlineStyle />
     </div>
-  );
+  )
 }
-
-export default App;
