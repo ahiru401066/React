7cc6aa8d5fc6cd86279fe035af9ccf00fc1db004
Tomoya <ichikawatomoya0705@gmail.com>
2024-11-06 23:09:50 +0900

    初期実装完了

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"; 
+import { ChildArea } from './Components/ChildArea';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+      <p>{text}</p>
     </div>
   );
 }
