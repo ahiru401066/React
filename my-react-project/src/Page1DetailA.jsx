ab459f03558f3d4910356e0810dec50df959ddd3
Tomoya <ichikawatomoya0705@gmail.com>
2024-11-10 10:34:35 +0900

    Linkを使わないページ遷移

--- my-react-project/src/Page1DetailA.jsx
+++ my-react-project/src/Page1DetailA.jsx
@@ -1,11 +1,16 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const { state } = useLocation();
+  const navigation = useNavigate();
   console.log(state);
+
+  const onClickBack = () => navigation(-1);
+
   return (
     <div>
       <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
