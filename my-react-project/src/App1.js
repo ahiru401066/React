95162303e11ac3e57203e5c6ec29442d88e95658
Tomoya <ichikawatomoya0705@gmail.com>
2024-11-10 13:44:29 +0900

    Organisumの作成1の完了

--- my-react-project/src/App1.js
+++ my-react-project/src/App1.js
@@ -1,5 +1,7 @@
 import { PrimaryButton } from "./Components1/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./Components1/atoms/button/SecondaryButton";
+import { SearchInput } from "./Components1/molecules/SearchInput";
+import { UserCard } from "./Components1/organisms/user/UserCard";
 import "./styles.css";
 
 export default function App1() {
@@ -8,6 +10,9 @@ export default function App1() {
       <h1>Hello World!</h1>
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>   
+      <br />
+      <SearchInput />
+      <UserCard />
     </div>
   );
 };
